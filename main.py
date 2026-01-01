3f3bda0239fa7f6922fc7529c87b5ef0da8b73ed
Ghost 47 <xrosalesportocarrero@gmail.com>
2025-12-31 23:29:31 -0500

    attempt 7

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def get_book_text(path_to_file):
-    with open("path_to_file") as f:
-        file_content = f.read()
-    return file_content
+def get_book_text(file_path):
+    with open(file_path) as file:
+        content = file.read()
+    return content
 
 def main():
-    get_book_text(~/frankenstein.txt)
+    get_book_text(books/frankenstein.txt)
 
 main()
