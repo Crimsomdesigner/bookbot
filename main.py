31fdd29d9b9d278cb864ffd47ea31e376b681382
Ghost 47 <xrosalesportocarrero@gmail.com>
2025-12-31 23:47:57 -0500

    adding word counts

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def get_book_text(file_path):
         content = file.read()
     return content
 
+def get_word_counts(File_text):
+    counts = len(File_text.split())
+    return counts
+
 def main():
     file_path = 'books/frankenstein.txt'
-    File_contents = get_book_text(file_path)
-    print (File_contents)
+    file_contents = get_book_text(file_path)
+    num_words = get_word_counts(file_contents)
+    print("Found {num_words} total words")
+    
 main()
