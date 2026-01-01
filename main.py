ad6d9c9570ce1c44ec6da88beac37ff34ce494f5
Ghost 47 <xrosalesportocarrero@gmail.com>
2025-12-31 23:49:15 -0500

    attempt 1

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def main():
     file_path = 'books/frankenstein.txt'
     file_contents = get_book_text(file_path)
     num_words = get_word_counts(file_contents)
-    print("Found {num_words} total words")
-    
+    print("Found " + num_words + " total words")
+
 main()
