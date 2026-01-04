49a8dccd3f3a340711dfe3dd14ea27d6648f14e4
Ghost 47 <xrosalesportocarrero@gmail.com>
2026-01-04 16:18:38 -0500

    Updating get_char_counts function

--- stats.py
+++ stats.py
@@ -3,20 +3,12 @@ def get_word_counts(File_text):
     return counts
 
 def get_char_counts(File_text):
-    t_count = 0
-    p_count = 0
-    c_count = 0
+    char_dict = {}
     char_list = list(File_text.lower())
-    for c in range(0, len(char_list)):
-        if char_list[c] == "t":
-            t_count += 1
-        elif char_list[c] == "p":
-            p_count += 1
-        elif char_list[c] == "c":
-            c_count += 1
-    return {
-        "t": t_count,
-        "p": p_count,
-        "c": c_count,
-    }
+    for character in char_list:
+        if character in char_dict:
+            char_dict[character] += 1
+        else:
+            char_dict[character] = 1
+    return char_dict
     
