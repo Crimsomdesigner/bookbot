1be8dc7c8694a1d430d382c88f926826c0c5fbf1
Ghost 47 <xrosalesportocarrero@gmail.com>
2026-01-04 18:33:43 -0500

    updating character dict to be alphabetic

--- stats.py
+++ stats.py
@@ -6,10 +6,11 @@ def get_char_counts(File_text):
     char_dict = {}
     char_list = list(File_text.lower())
     for character in char_list:
-        if character in char_dict:
-            char_dict[character] += 1
-        else:
-            char_dict[character] = 1
+        if character.isalpha():
+            if character in char_dict:
+                char_dict[character] += 1
+            else:
+                char_dict[character] = 1
     return char_dict
 
 def sort_on(dict):
