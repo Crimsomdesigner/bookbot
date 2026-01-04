64e0ce7d9b87cdf2d963ad75337626c96c129a72
Ghost 47 <xrosalesportocarrero@gmail.com>
2026-01-04 18:23:29 -0500

    updating file path

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ def get_book_text(file_path):
 from stats import *
 
 def main():
-    #file_path =
-    #file_contents = get_book_text(file_path)
-    #num_words = get_word_counts(file_contents)
-    #character_count_dict = get_char_counts(file_contents)
-    #char_sorted_list = sort_on(character_count_dict)
-    #print("============ BOOKBOT ============")
-    #print(f"Analyzing book found at {file_path}...")
-    #print("----------- Word Count ----------")
-    #print(f"Found {num_words} total words")
-    #print("--------- Character Count -------")
-    #for d in char_sorted_list:
-        #for char, count in d.items():
-            #print(f"{char}: {count}")
-    #print("============= END ===============")
+    file_path = sys.argv[1]
+    file_contents = get_book_text(file_path)
+    num_words = get_word_counts(file_contents)
+    character_count_dict = get_char_counts(file_contents)
+    char_sorted_list = sort_on(character_count_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for d in char_sorted_list:
+        for char, count in d.items():
+            print(f"{char}: {count}")
+    print("============= END ===============")
     print(sys.argv)
     print("Usage: python3 main.py <path_to_book>")
-    print(sys.exit(1))
+    sys.exit(1)
 
 main()
